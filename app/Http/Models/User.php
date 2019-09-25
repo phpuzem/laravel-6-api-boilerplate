185a1a9319e9d26052ba0c5ba3f0855e5fa13343
Halil COŞDU <halilcosdu@gmail.com>
2019-09-25 23:47:36 +0300

    AuthService added.

--- app/Http/Models/User.php
+++ app/Http/Models/User.php
@@ -1,12 +1,16 @@
 <?php
 
-namespace App;
+namespace App\Http\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
+/**
+ * Class User
+ * @package App\Http\Models
+ */
 class User extends Authenticatable
 {
     use Notifiable, HasRoles, HasApiTokens;
@@ -40,4 +44,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @param $value
+     */
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
 }
---
+++ app/Services/Auth/AuthService.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Services;
+
+use App\Http\Models\User;
+use App\Http\Requests\Request;
+use Illuminate\Auth\AuthManager;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Auth\Passwords\PasswordBrokerManager;
+use Illuminate\Support\Facades\Password;
+
+/**
+ * Class AuthService
+ * @package App\Services
+ */
+class AuthService
+{
+    /**
+     * @var \Illuminate\Auth\AuthManager
+     */
+    protected $authManager;
+    /**
+     * @var \Illuminate\Auth\Passwords\PasswordBrokerManager
+     */
+    protected $passwordBrokerManager;
+    /**
+     * @var \App\Http\Models\User
+     */
+    protected $user;
+
+    /**
+     * AuthService constructor.
+     *
+     * @param \Illuminate\Auth\AuthManager $authManager
+     * @param \Illuminate\Auth\Passwords\PasswordBrokerManager $passwordBrokerManager
+     * @param \App\Http\Models\User $user
+     */
+    public function __construct(AuthManager $authManager, PasswordBrokerManager $passwordBrokerManager, User $user)
+    {
+        $this->authManager           = $authManager;
+        $this->passwordBrokerManager = $passwordBrokerManager;
+        $this->user                  = $user;
+    }
+
+    /**
+     * @param $credentials
+     *
+     * @return bool
+     */
+    public function authenticate($credentials)
+    {
+        return $this->authManager
+            ->guard()
+            ->attempt($credentials);
+    }
+
+    /**
+     * @param \App\Http\Requests\Request $request
+     *
+     * @return mixed
+     */
+    public function register(Request $request)
+    {
+        return $this->user->create(
+            $request->only('name', 'email', 'password')
+        );
+    }
+
+    /**
+     * @param \App\Http\Requests\Request $request
+     * @param \App\Http\Models\User|null $user
+     *
+     * @return \Laravel\Passport\PersonalAccessTokenResult
+     */
+    public function generateAccessToken(Request $request, User $user = null)
+    {
+        return $request->user() ? $request->user()->createToken('Personal Access Token') : $user->createToken('Personal Access Token');
+    }
+
+    /**
+     * @param \App\Http\Requests\Request $request
+     *
+     * @return bool
+     */
+    public function sendForgotPasswordResetLink(Request $request)
+    {
+        $response = $this->passwordBrokerManager->sendResetLink([
+            $request->only('email'),
+        ]);
+
+        return $response === Password::RESET_LINK_SENT;
+    }
+
+    /**
+     * @param \App\Http\Requests\Request $request
+     *
+     * @return bool
+     */
+    public function resetPassword(Request $request)
+    {
+        $response = $this->passwordBrokerManager->reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, $password) {
+                $user->password = $password;
+                $user->save();
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $response === Password::PASSWORD_RESET;
+
+    }
+
+    /**
+     * @param \App\Http\Requests\Request $request
+     *
+     * @return mixed
+     */
+    public function logout(Request $request)
+    {
+        return $request->user()->token()->revoke();
+    }
+
+}
