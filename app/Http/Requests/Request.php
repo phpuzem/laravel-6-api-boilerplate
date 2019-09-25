33bebb84dd6aa76a7d1f2dcfdcd626d3d454977f
Halil COŞDU <halilcosdu@gmail.com>
2019-09-25 22:23:06 +0300

    Abstract form request added.

---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Services\JsonResponseService;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest as LaravelFormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+/**
+ * Class Request
+ * @package App\Http\Requests
+ */
+abstract class Request extends LaravelFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    abstract public function authorize();
+
+    /**
+     * @param \Illuminate\Contracts\Validation\Validator $validator
+     */
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            (new JsonResponseService())->fail([
+                'errors' => $validator->errors()->messages(),
+            ])
+        );
+    }
+}
