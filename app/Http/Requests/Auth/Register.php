a72a0b410df11bbd2c388ed7671ac03b6229cfea
Halil COŞDU <halilcosdu@gmail.com>
2019-10-13 19:45:51 +0300

    User added.

--- app/Http/Requests/Auth/Register.php
+++ app/Http/Requests/Auth/Register.php
@@ -26,7 +26,7 @@ class Register extends Request
         return [
             'username' => 'required|unique:users',
             'email'    => 'required|email|unique:users',
-            'password' => 'required|confirmed',
+            'password' => 'required|confirmed|min6',
         ];
     }
 }
