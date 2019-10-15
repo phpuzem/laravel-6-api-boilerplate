74db0a33599d540b52ab8bc2a8dc4a78caa6c90d
Halil COŞDU <halilcosdu@gmail.com>
2019-10-15 21:14:33 +0300

    User updated.

--- app/Providers/RepositoryProvider.php
+++ app/Providers/RepositoryProvider.php
@@ -5,9 +5,11 @@ namespace App\Providers;
 use App\Contracts\JobContract;
 use App\Contracts\PermissionContract;
 use App\Contracts\RaceContract;
+use App\Contracts\UserContract;
 use App\Repositories\Eloquent\JobRepository;
 use App\Repositories\Eloquent\PermissionRepository;
 use App\Repositories\Eloquent\RaceRepository;
+use App\Repositories\Eloquent\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryProvider extends ServiceProvider
@@ -32,5 +34,6 @@ class RepositoryProvider extends ServiceProvider
         $this->app->bind(PermissionContract::class, PermissionRepository::class);
         $this->app->bind(JobContract::class, JobRepository::class);
         $this->app->bind(RaceContract::class, RaceRepository::class);
+        $this->app->bind(UserContract::class, UserRepository::class);
     }
 }
