6a24738e3dddcb3809de3d9333513d4916fd800c
Halil COŞDU <halilcosdu@gmail.com>
2019-10-16 20:32:41 +0300

    Stat added.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RacesTableSeeder::class);
         $this->call(JobsTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
+       // $this->call(UsersTableSeeder::class);
     }
 }
